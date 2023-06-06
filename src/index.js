af8caa5aba04618601e307f86a15d335441ca8c8
Abril <abrilv29@gmail.com>
2023-06-05 20:57:02 -0600

    Selectores en CSS name y id

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+// funcion para limpiar un button con javascript
+
+
+const textArea = document.querySelector('textarea');
+const botonLimpiar = document.getElementById("reset-button");
+botonLimpiar.addEventListener("click", limpiarBtn);
+
+function limpiarBtn(){
+  console.log(textArea.value);
+   textArea.value ='';
+
+  }
